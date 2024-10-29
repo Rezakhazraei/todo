9af6ccd87c5ed6c373833b04fdc1e113a2ec6ca9
Abolfazlkhazraei <t3khab00@students.oamk.fi>
2024-10-29 14:25:49 +0200

    Part 3 - Todo

--- todo/server/index.test.js
+++ todo/server/index.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable jest/valid-expect */
 import { expect } from "chai"
 
@@ -7,9 +8,8 @@ describe("GET Tasks", () => {
         const data = await response.json()
 
         expect(response.status).to.equal(200)
-        // eslint-disable-next-line no-unused-expressions
-        expect(data).to.be.an("array").that.is.not.empty
-        expect(data[0].to.include.all.keys("id","description"))
+        expect(data).to.be.an("array").that.is.not.empty;
+        expect(data[0]).to.include.all.keys("id","description")
     })
 })
 
@@ -27,5 +27,42 @@ describe("POST task",() => {
         expect(response.status).to.equal(200)
         expect(data).to.be.an('object')
         expect(data).to.include.all.keys('id')
+    })
+
+    it("should not post a task without description", async() => {
+        const response = await fetch(base_url + "create", {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({"description":null})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(500)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('error')
+    })
+})
+
+describe("DELETE task",() => {
+    const base_url = "http://localhost:3001/";
+    it("should delete a task", async() => {
+        const response = await fetch(base_url + "delete/1", {
+            method: 'DELETE'
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(200)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id')
+    })
+
+    it("should not delete a task with SQL injection", async() => {
+        const response = await fetch(base_url + "delete/id=0 or id > 0", {
+            method: 'DELETE'
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(500)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('error')
     })
 })
